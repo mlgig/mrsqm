0309de37f545974ecc82a5c5ea41d18d0874b1ca
lnthach <lenguyenthach141@gmail.com>
2020-11-05 22:07:35 +0000

    minor update

--- example/example.py
+++ example/example.py
@@ -2,26 +2,40 @@ from mrsqm import MrSQMClassifier
 from sklearn import metrics
 import util
 
-import subprocess
-
 import logging
 import sys
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-def run_sfa(itrain, itest, otrain, otest):
-    subprocess.call(['java', '-jar', 'TestSFA-all.jar', itrain, itest, otrain, otest])
-
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 def basic_example():
 
     X_train,y_train = util.load_from_arff_to_dataframe("data/Coffee/Coffee_TRAIN.arff")
     X_test,y_test = util.load_from_arff_to_dataframe("data/Coffee/Coffee_TEST.arff")
 
-    clf = MrSQMClassifier(strat = 'RS') # train MrSQM-RS
+    # train MrSQM-R with SAX only
+    clf = MrSQMClassifier(strat = 'R')
     clf.fit(X_train,y_train)
-
     predicted = clf.predict(X_test)
-    print(metrics.accuracy_score(y_test, predicted))
+    print("MrSQM-R accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
+
+    # train MrSQM-RS with SAX only
+    clf = MrSQMClassifier(strat = 'RS') # default 
+    clf.fit(X_train,y_train)
+    predicted = clf.predict(X_test)
+    print("MrSQM-RS accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
+
+    # train MrSQM-S with SAX only
+    clf = MrSQMClassifier(strat = 'S')
+    clf.fit(X_train,y_train)
+    predicted = clf.predict(X_test)
+    print("MrSQM-S accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
+
+    # train MrSQM-SR with SAX only
+    clf = MrSQMClassifier(strat = 'SR')
+    clf.fit(X_train,y_train)
+    predicted = clf.predict(X_test)
+    print("MrSQM-SR accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
+
+    
 
 def mrsqm_with_sfa():
     X_train,y_train = util.load_from_arff_to_dataframe("data/Coffee/Coffee_TRAIN.arff")
@@ -32,15 +46,20 @@ def mrsqm_with_sfa():
     itest = "data/Coffee/Coffee_TEST.txt"
     otrain = 'sfa.train'
     otest = 'sfa.test'
-    run_sfa(itrain, itest, otrain, otest)
+    util.sfa_transform(itrain, itest, otrain, otest)
 
+    # train MrSQM-RS with SFA only
+    clf = MrSQMClassifier(use_sax = False) # train MrSQM-RS
+    clf.fit(X_train,y_train, ext_rep = otrain) # use ext_rep to add sfa transform
+    predicted = clf.predict(X_test, ext_rep = otest) # use ext_rep to add sfa transform
+    print("MrSQM-RS with SFA only accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
 
-    clf = MrSQMClassifier(strat = 'RS') # train MrSQM-RS
-    clf.fit(X_train,y_train, ext_reps = otrain) # use ext_reps to add sfa transform
-
-    predicted = clf.predict(X_test, ext_reps = otest) # use ext_reps to add sfa transform
-
-    print(metrics.accuracy_score(y_test, predicted))
+    # train MrSQM-RS with both SAX and SFA
+    clf = MrSQMClassifier() # train MrSQM-RS
+    clf.fit(X_train,y_train, ext_rep = otrain) # use ext_rep to add sfa transform
+    predicted = clf.predict(X_test, ext_rep = otest) # use ext_rep to add sfa transform
+    print("MrSQM-RS with both SAX and SFA accuracy: " + str(metrics.accuracy_score(y_test, predicted)))
 
 if __name__ == "__main__":
+    basic_example()
     mrsqm_with_sfa()
