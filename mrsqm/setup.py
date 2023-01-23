d4b5e1443dd5b6434e85746994a210cdc540f708
GI <heerme@users.noreply.github.com>
2023-01-23 16:33:54 +0000

    Removed "-mfpmath=both" that causes a gcc compilation error.

--- mrsqm/setup.py
+++ mrsqm/setup.py
@@ -15,7 +15,7 @@ setup(
             sources=["mrsqm_wrapper.pyx","sfa/MFT.cpp","sfa/DFT.cpp","sfa/SFA.cpp","sfa/TimeSeries.cpp"], # the Cython source and
                                                   # additional C++ source files
             #sources=["mrsqm_wrapper.pyx"],
-            extra_compile_args=["-Wall", "-Ofast", "-g", "-std=c++11", "-mfpmath=both", "-ffast-math"],
+            extra_compile_args=["-Wall", "-Ofast", "-g", "-std=c++11",  "-ffast-math"],
             extra_link_args=["-lfftw3", "-lm", "-L/opt/local/lib"],           
             language="c++",                        # generate and compile C++ code
 
