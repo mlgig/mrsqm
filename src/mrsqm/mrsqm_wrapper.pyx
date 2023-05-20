1c3bdd367774ac16b7fa9601b7f88503e6c98b6b
lnthach <lenguyenthach141@gmail.com>
2023-05-20 15:35:51 +0100

    use transformer inside classifier

--- src/mrsqm/mrsqm_wrapper.pyx
+++ src/mrsqm/mrsqm_wrapper.pyx
@@ -494,13 +494,34 @@ class MrSQMTransformer:
         return X_transform
 
     
+    def get_saliency_map(self, ts, coefs):        
 
+        is_multiclass = len(self.classes_) > 2
+        weighted_ts = np.zeros((len(self.classes_), len(ts)))
+
+        fi = 0
+        for cfg, features in zip(self.config, self.sequences):
+            if cfg['method'] == 'sax':
+                ps = PySAX(cfg['window'], cfg['word'], cfg['alphabet'])
+                if is_multiclass:
+                    for ci, cl in enumerate(self.classes_):
+                        weighted_ts[ci, :] += ps.map_weighted_patterns(
+                            ts, features, coefs[ci, fi:(fi+len(features))])
+                else:
+                    # because classes_[1] is the positive class
+                    weighted_ts[1, :] += ps.map_weighted_patterns(
+                        ts, features, coefs[0, fi:(fi+len(features))])
+
+            fi += len(features)
+        if not is_multiclass:
+            weighted_ts[0, :] = -weighted_ts[1, :]
+        return weighted_ts
 
 
 
 ######################### MrSQM Classifier #########################
 
-class MrSQMClassifier:    
+class MrSQMClassifier:
     '''     
     Overview: MrSQM is an efficient time series classifier utilizing symbolic representations of time series. MrSQM implements four different feature selection strategies (R,S,RS,SR) that can quickly select subsequences from multiple symbolic representations of time series data.
     def __init__(self, strat = 'RS', features_per_rep = 500, selection_per_rep = 2000, nsax = 1, nsfa = 0, custom_config=None, random_state = None, sfa_norm = True):
@@ -518,248 +539,12 @@ class MrSQMClassifier:
     ts_norm             : time series normalisation (standardisation). By default set to 'True'.
 
     '''
-
-    def __init__(self, strat = 'RS', features_per_rep = 500, selection_per_rep = 2000, nsax = 1, nsfa = 0, custom_config=None, random_state = None, sfa_norm = True):
-        
-
-        self.nsax = nsax
-        self.nsfa = nsfa
-
-        self.sfa_norm = sfa_norm
-        if random_state is not None:
-            np.random.seed(random_state)
-        # self.random_state = (
-        #     np.int32(random_state) if isinstance(random_state, int) else None
-        # )
-
-        if custom_config is None:
-            self.config = [] # http://effbot.org/zone/default-values.htm
-        else:
-            self.config = custom_config
-
-        self.strat = strat   
-
-        # all the unique labels in the data
-        # in case of binary data the first one is always the negative class
-        self.classes_ = []
-        self.clf = None # scikit-learn model       
-
-        self.fpr = features_per_rep
-        self.spr = selection_per_rep
-        
-        self.filters = [] # feature filters (one filter for a rep) for test data transformation
-
-        debug_logging("Initialize MrSQM Classifier.")
-        debug_logging("Feature Selection Strategy: " + strat)
-        debug_logging("SAX Reps: " + str(self.nsax) + "x")
-        debug_logging("SFA Reps: " + str(self.nsfa) + "x")
-        debug_logging("Number of features per rep: " + str(self.fpr))
-        debug_logging("Number of candidates per rep (only for SR and RS):" + str(self.spr))
-        
-     
-
-    def create_pars(self, min_ws, max_ws, xrep, random_sampling=False, is_sfa=False):
-        pars = []      
-        if xrep > 0:      
-            if random_sampling:    
-                debug_logging("Sampling window size, word length, and alphabet size.")       
-                ws_choices = [int(2**(w/xrep)) for w in range(3*xrep,xrep*int(np.log2(max_ws))+ 1)]            
-                wl_choices = [6,8,10,12,14,16]
-                if is_sfa:
-                    wl_choices = [6,8,10,12,14] # can't handle 16x6 case
-                alphabet_choices = [3,4,5,6]
-
-                nrep = xrep*int(np.log2(max_ws))                
-                for w in range(nrep):
-                    pars.append([np.random.choice(ws_choices) , np.random.choice(wl_choices), np.random.choice(alphabet_choices)])
-            else:
-                #debug_logging("Doubling the window while fixing word length and alphabet size.")                   
-                #pars = [[int(2**(w/xrep)),8,4] for w in range(3*xrep,xrep*int(np.log2(max_ws))+ 1)]     
-                #if not is_sfa:
-                pars = [[w,8,4] for w in range(8,max_ws,int(np.sqrt(max_ws)))]     
-
-        debug_logging("Symbolic Parameters: " + str(pars))      
-            
-        
-        return pars            
-            
-            
-
-    def transform_time_series(self, ts_x):
-        debug_logging("Transform time series to symbolic representations.")
-        
-        multi_tssr = []   
-
-        ts_x_array = from_nested_to_2d_array(ts_x).values
-        
-     
-        if not self.config:
-            self.config = []
-            
-            min_ws = 16
-            min_len = max_len = len(ts_x.iloc[0, 0])
-            for a in ts_x.iloc[:, 0]:
-                min_len = min(min_len, len(a)) 
-                max_len = max(max_len, len(a))
-            max_ws = (min_len + max_len)//2            
-            
-            
-            pars = self.create_pars(min_ws, max_ws, self.nsax, random_sampling=True, is_sfa=False)            
-            for p in pars:
-                self.config.append(
-                        {'method': 'sax', 'window': p[0], 'word': p[1], 'alphabet': p[2], 
-                        # 'dilation': np.int32(2 ** np.random.uniform(0, np.log2((min_len - 1) / (p[0] - 1))))})
-                        'dilation': 1})
-            
-            pars = self.create_pars(min_ws, max_ws, self.nsfa, random_sampling=True, is_sfa=True)            
-            for p in pars:
-                self.config.append(
-                        {'method': 'sfa', 'window': p[0], 'word': p[1], 'alphabet': p[2] , 'normSFA': False, 'normTS': self.sfa_norm
-                        })        
-
-        
-        for cfg in self.config:
-            for i in range(ts_x.shape[1]):
-                tssr = []
-
-                if cfg['method'] == 'sax':  # convert time series to SAX                    
-                    ps = PySAX(cfg['window'], cfg['word'], cfg['alphabet'], cfg['dilation'])
-                    for ts in ts_x.iloc[:,i]:
-                        sr = ps.timeseries2SAXseq(ts)
-                        tssr.append(sr)
-                elif  cfg['method'] == 'sfa':
-                    if 'signature' not in cfg:
-                        cfg['signature'] = PySFA(cfg['window'], cfg['word'], cfg['alphabet'], cfg['normSFA'], cfg['normTS']).fit(ts_x_array)
-                    
-                    tssr = cfg['signature'].transform(ts_x_array)
-                multi_tssr.append(tssr)        
-
-        return multi_tssr
-  
-    def sample_random_sequences(self, seqs, min_length, max_length, max_n_seq):  
-                
-        output = set()
-        splitted_seqs = [s.split(b' ') for s in seqs]
-        n_input = len(seqs)       
-
-        # while len(output) < n_seq: #infinity loop if sequences are too alike
-        for i in range(0, max_n_seq):
-            did = randint(0,n_input)
-            wid = randint(0,len(splitted_seqs[did]))
-            word = splitted_seqs[did][wid]
-            
-            s_length = randint(min_length, min(len(word) + 1, max_length + 1))
-            start = randint(0,len(word) - s_length + 1)        
-            sampled = word[start:(start + s_length)]
-            output.add(sampled)
-        
-        return list(output)
-
-    def feature_selection_on_train(self, mr_seqs, y):
-        debug_logging("Compute train data in subsequence space.")
-        full_fm = []
-        self.filters = []
-
-        for i in range(0,len(mr_seqs)):
-        #for rep, seq_features in zip(mr_seqs, self.sequences):            
-            rep = mr_seqs[i]
-            seq_features = self.sequences[i]
-            fm = np.zeros((len(rep), len(seq_features)),dtype = np.int32)
-            ft = PyFeatureTrie(seq_features)
-            for ii,s in enumerate(rep):
-                fm[ii,:] = ft.search(s)            
-            fm = fm > 0 # binary only
-
-            fs = SelectKBest(chi2, k=min(self.fpr, fm.shape[1]))
-            if self.strat == 'RS':
-                debug_logging("Filter subsequences of this representation with chi2 (only with RS).")
-                fm = fs.fit_transform(fm, y)
-                self.sequences[i] = [seq_features[ii] for ii in fs.get_support(indices=True)]               
-
-
-            self.filters.append(fs)
-            full_fm.append(fm)
-
-
-        full_fm = np.hstack(full_fm)
-
-        #self.final_vt = VarianceThreshold()
-        #return self.final_vt.fit_transform(full_fm)
-        return full_fm
-
-    def feature_selection_on_test(self, mr_seqs):
-        debug_logging("Compute test data in subsequence space.")
-        full_fm = []
-        
-
-        for rep, seq_features, fs in zip(mr_seqs, self.sequences, self.filters):            
-            fm = np.zeros((len(rep), len(seq_features)),dtype = np.int32)
-            ft = PyFeatureTrie(seq_features)
-            for i,s in enumerate(rep):
-                fm[i,:] = ft.search(s)
-            fm = fm > 0 # binary only
-
-            # if self.strat == 'RS':
-            #     fm = fs.transform(fm)        
-            full_fm.append(fm)
-
-
-        full_fm = np.hstack(full_fm)
-        #return self.final_vt.transform(full_fm)
-        return full_fm
+    def __init__(self, strat = 'RS', features_per_rep = 500, selection_per_rep = 2000, nsax = 1, nsfa = 0, custom_config=None, random_state = None, sfa_norm = True):        
+        self.transformer = MrSQMTransformer(strat = strat, features_per_rep = features_per_rep, selection_per_rep = selection_per_rep, nsax = nsax, nsfa = nsfa, custom_config = custom_config, random_state = random_state, sfa_norm = sfa_norm)
     
-
-    def mine(self,rep, int_y):        
-        mined_subs = []
-        if self.strat == 'S':
-            debug_logging("Select " + str(self.fpr) + " discrimative subsequences with SQM.")
-            miner = PySQM(self.fpr,0.0)
-            mined_subs = miner.mine(rep, int_y)
-
-        elif self.strat == 'SR':
-            debug_logging("Select " + str(self.spr) + " discrimative subsequences with SQM.")
-            miner = PySQM(self.spr,0.0)
-            mined_subs = miner.mine(rep, int_y)
-            debug_logging("Randomly pick " + str(self.fpr) + " subsequences from the list.")
-            mined_subs = np.random.permutation(mined_subs)[:self.fpr].tolist()
-
-        elif self.strat == 'R':
-            debug_logging("Random sampling " + str(self.fpr) + " subsequences from this symbolic representation.")
-            mined_subs = self.sample_random_sequences(rep,3,16,self.fpr)
-        elif self.strat == 'RS':
-            debug_logging("Random sampling " + str(self.spr) + " subsequences from this symbolic representation.")
-            mined_subs = self.sample_random_sequences(rep,3,16,self.spr)
-
-        debug_logging("Found " + str(len(mined_subs)) + " unique subsequences.")
-        return mined_subs
-
-
-
     def fit(self, X, y):
-        debug_logging("Fit training data.")
-        self.classes_ = np.unique(y) #because sklearn also uses np.unique
-
-        int_y = [np.where(self.classes_ == c)[0][0] for c in y]
-
-        self.sequences = []
-
-        debug_logging("Search for subsequences.")        
-        mr_seqs = self.transform_time_series(X)
-        
-        
-        
-        for rep in mr_seqs:
-            mined = self.mine(rep,int_y)
-            self.sequences.append(mined)
-
-
-    
-        # first computing the feature vectors
-        # then fit the new data to a logistic regression model
-        
-        debug_logging("Compute feature vectors.")
-        train_x = self.feature_selection_on_train(mr_seqs, int_y)
-        
+        debug_logging("Fit training data.")        
+        train_x = self.transformer.fit_transform(X,y)
         debug_logging("Fit logistic regression model.")
         self.clf = LogisticRegression(solver='newton-cg',multi_class = 'multinomial', class_weight='balanced').fit(train_x, y)        
         # self.clf = RidgeClassifierCV(alphas = np.logspace(-3, 3, 10), normalize = True).fit(train_x, y)        
@@ -768,53 +553,16 @@ class MrSQMClassifier:
         return self
 
 
-    def predict_proba(self, X): 
-        mr_seqs = self.transform_time_series(X)       
-        test_x = self.feature_selection_on_test(mr_seqs)
-        return self.clf.predict_proba(test_x) 
+    def predict_proba(self, X):         
+        return self.clf.predict_proba(self.transformer.transform(X)) 
 
-    def predict(self, X):
-        mr_seqs = self.transform_time_series(X)       
-        test_x = self.feature_selection_on_test(mr_seqs)
-        return self.clf.predict(test_x)
+    def predict(self, X):        
+        return self.clf.predict(self.transformer.transform(X))
 
-    def decision_function(self, X):
-        mr_seqs = self.transform_time_series(X)       
-        test_x = self.feature_selection_on_test(mr_seqs)
-        return self.clf.decision_function(test_x)
+    def decision_function(self, X):        
+        return self.clf.decision_function(self.transformer.transform(X))
 
-    def get_saliency_map(self, ts):        
+    def get_saliency_map(self, ts):
+        return self.transformer.get_saliency_map(ts,self.clf.coef_)
 
-        is_multiclass = len(self.classes_) > 2
-        weighted_ts = np.zeros((len(self.classes_), len(ts)))
-
-        fi = 0
-        for cfg, features in zip(self.config, self.sequences):
-            if cfg['method'] == 'sax':
-                ps = PySAX(cfg['window'], cfg['word'], cfg['alphabet'])
-                if is_multiclass:
-                    for ci, cl in enumerate(self.classes_):
-                        weighted_ts[ci, :] += ps.map_weighted_patterns(
-                            ts, features, self.clf.coef_[ci, fi:(fi+len(features))])
-                else:
-                    # because classes_[1] is the positive class
-                    weighted_ts[1, :] += ps.map_weighted_patterns(
-                        ts, features, self.clf.coef_[0, fi:(fi+len(features))])
-
-            fi += len(features)
-        if not is_multiclass:
-            weighted_ts[0, :] = -weighted_ts[1, :]
-        return weighted_ts
-        
-
-
-
-
-
- 
-
-
-
-
-
-
+    
